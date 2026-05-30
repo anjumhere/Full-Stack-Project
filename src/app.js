d598b00567e4ad61266a6a2e9f3c32054e67f432
Adnan Anjum <anjumcode12@gmail.com>
2026-05-31 00:28:24 +0500

    Commit current app and utility updates

--- src/app.js
+++ src/app.js
@@ -3,15 +3,15 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 const app = express();
 
-export { app };
-
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
-    Credentials: true,
+    credentials: true,
   }),
 );
 app.use(express.json({ limit: '16kb' })); // to handle data comming from forms.
 app.use(express.urlencoded({ extended: true, limit: '16kb' })); // handle url data , url encoder
 app.use(express.static('public'));
 app.use(cookieParser());
+
+export { app };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,19 +1,19 @@
 const asyncHandler = (requestHandler) => {
   (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
-      next(error),
-    );
+    Promise.resolve(requestHandler(req, res, next)).catch((error) => {
+      res.next(error);
+    });
   };
 };
 export { asyncHandler };
 
-const asyncHandler = (fn) => async () => {
-  try {
-    await fn(req, res, next);
-  } catch (error) {
-    res.status(error.code || 500).json({
-      success: false,
-      message: error.message,
-    });
-  }
-};
+// const requestHandler =(fn)=> async(req,res,next)=>{
+// try {
+//   fn(req,res,next)
+// } catch (error) {
+//   req.status(error.code || 500).json({
+//     success: false,
+//     message: error.message
+//   })
+// }
+// }
