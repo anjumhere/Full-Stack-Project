38ab7e0979ed370d2f92733e0cc727a16f2d5023
Adnan Anjum <anjumcode12@gmail.com>
2026-06-05 12:47:44 +0500

    add controller , completed getting user from frontend , validation , checked for images'

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,17 +1,43 @@
 import asyncHandler from '../utils/asyncHandler.js';
+import { ApiError } from '../utils/ApiError.js';
+import { User } from '../utils/ApiError.js';
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
-  // validation - no empty
-  // check if user already exsists : username/email
-  // check for images - check for avatar( compulsory)
-  // upload them to cloudinary, avatar
-  // create user-object - create entry in db
-  // remove password and refresh token fields from response
-  // check for user creation if received null or created
-  // return response
+  // validation - if not empty
+  // check if user or email already exsists
+  // check for images / check for avatar which is required
+  // upload them to cloudinary which would return a url
+  // create a user object - create entry in mongodb
+  // remove password and refresh token from field
+  // remove password and refresh token from field
+  // check if user is created / check if not null
 
-  const { username, email, fullName, password } = req.body;
-  console.log('email', email, 'password', password);
+  const { username, fullName, email, password } = req.body;
+  console.log(`Email = ${email}`);
+
+  // validation
+  if (
+    [fullName, username, email, password].some((field) => field?.trim() === '')
+  ) {
+    throw new ApiError(400, 'All fields are required');
+  }
+
+  //check if user and email already exsists
+
+  const exsistedUser = User.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (exsistedUser) {
+    throw new ApiError(409, 'User with email or username already exsits');
+  }
+  //check if avatar and cover image are available
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  if (!avatarLocalPath) {
+    throw new ApiError(400, 'avatar image required');
+  }
+
+  //upload them to cloudinary
 });
 
 export { registerUser };
